2bcfdbd1674a769d491ad04a98b75b15137e667f
BuyiswaM <buyiswamotaung96@gmail.com>
2024-07-17 14:39:58 +0200

    Home and product props added

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter';
+import Home from './Components/Home';
+import Product from './Components/Product';
 
 function App() {
   return (
 
     <div className="App">
+      <Home/>
+      <Product/>
       <Counter/>
     </div>
   );
