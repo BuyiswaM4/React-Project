6ca504ed04d5af6073155b5e93d33f52b62a5226
BuyiswaM <buyiswamotaung96@gmail.com>
2024-07-23 14:08:08 +0200

    navbar created, style.css added

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Counter from './Components/Counter';
 import Home from './Components/Home';
 import Product from './Components/Product';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
 
     <div className="App">
+      <Navbar/>
       <Home/>
       <Product/>
       <Counter/>
