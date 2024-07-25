dfa397b7710231556169af1fef4a2454355ad1bc
BuyiswaM <buyiswamotaung96@gmail.com>
2024-07-25 12:06:52 +0200

    Navbar created and nav.css added

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,17 +4,22 @@ import Counter from './Components/Counter';
 import Home from './Components/Home';
 import Product from './Components/Product';
 import Navbar from './Components/Navbar';
-
+import About from './Components/About';
+import Contact from './Components/Contact';
+import {BrowserRouter as Router, Routes} from 'react-dom'
 function App() {
   return (
-
+    <Router>
     <div className="App">
       <Navbar/>
-      <Home/>
-      <Product/>
-      <Counter/>
+      <Routes>
+        <Route path ='/' element={<Home/>}/>
+        <Route path ='/about' element={<About/>}/>
+        <Route path ='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
-  );
+    </Router>
+  )
 }
 
 export default App;
